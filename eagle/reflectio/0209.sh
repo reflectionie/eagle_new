9a17daea98724a73593343ebef114de8dd214fb8
reflectionie <reflectionie@gmail.com>
2025-02-09 12:42:27 +0900

    update scripts

--- eagle/reflectio/0209.sh
+++ eagle/reflectio/0209.sh
@@ -1,4 +1,4 @@
-cd ..
+cd ../..
 
 PYTHONPATH=. ACCELERATE_MIXED_PRECISION=bf16 accelerate launch --multi_gpu --num_processes 4 --mixed_precision bf16 -m eagle.train.mainv2 --tmpdir ./eagle/reflectio/train_data/ --cpdir ./eagle/reflectio/checkpoints/ --basepath ./base_model/Meta-Llama-3-8B-Instruct/ --configpath ./eagle/train/EAGLE-LLaMA3-Instruct-8B.json --decision-method eagle --bs 6 --ckpt_path /home/5/uu02155/data/llama/hass_new/reflectio/li/EAGLE-LLaMA3-Instruct-8B
 
